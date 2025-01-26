a40d6be9a007e6a15e0753f27b3c177f8bc0c28a
Your Name <you@example.com>
2025-01-26 14:57:44 +0200

    Answer Project 1

---
+++ Assignments06_OOp/First Project/3DPoint.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments06_OOp.First_Project
+{
+    internal class _3DPoint
+    {
+        #region Attributes
+
+        private int x;
+        private int y;
+        private int z;
+
+        #endregion
+
+        #region Properties
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+        #endregion
+
+        #region Constrctors
+        public _3DPoint(int _x, int _y, int _z)
+        {
+            x= _x;
+            y= _y;
+            z= _z;
+
+        }
+        public _3DPoint(int _x, int _y) : this(_x, _y, 0)
+        {
+
+
+        }
+        public _3DPoint(int _x) : this(_x, 0, 0)
+        {
+
+
+        }
+        #endregion
+        public override string ToString()
+        {
+           return $"Point Coordinates:{x},{y},{z}";
+        }
+
+        #region 3.Read from the User the Coordinates for 2 points P1, P2 (Check the input using try Pares, Parse, Convert).
+        
+        public static _3DPoint Read3DPoint()
+        {
+            int x, y, z;
+            bool flag;
+
+            do
+            {
+                Console.Write(" Enter X:");
+                flag = int.TryParse(Console.ReadLine(), out x);
+
+            } while (!flag);
+            do
+            {
+                Console.Write(" Enter Y:");
+                flag = int.TryParse(Console.ReadLine(), out y);
+
+            } while (!flag);
+            do
+            {
+                Console.Write(" Enter Z:");
+                flag = int.TryParse(Console.ReadLine(), out z);
+
+            } while (!flag);
+
+            return new _3DPoint(x, y, z);
+        }
+
+        #endregion
+
+        #region 4.Try to use  == If(P1 == P2)   Does it work properly? 
+
+        public static bool operator ==(_3DPoint p1, _3DPoint p2)
+        {
+            return p1.x == p2.x && p1.y == p2.y && p1.z == p2.z;
+        }
+        public static bool operator !=(_3DPoint p1, _3DPoint p2)
+        {
+
+            return p1.x != p2.x || p1.y != p2.y || p1.z != p2.z;
+        }
+
+        #endregion
+
+        #region Define an array of points and sort this array based on X & Y coordinates
+        public static void Display(_3DPoint[] Nums)
+        {
+            for (int i = 0; i < Nums.Length; i++)
+                Console.WriteLine(Nums[i]);
+        }
+
+        public static void Sortting(_3DPoint[] Nums)
+        {
+            Array.Sort(Nums);
+        }
+
+        public int CompareTo(object? obj)
+        {
+            _3DPoint pp = (_3DPoint)obj;
+            if (x > pp.x)
+                return 1;
+            else if (x<pp.x)
+                return -1;
+            else
+            {
+                if (y>pp.y)
+                    return 1;
+                else if (y<pp.y)
+                    return -1;
+                else
+                {
+                    if (z > pp.z)
+                        return 1;
+                    else if (z<pp.z)
+                        return -1;
+                    return 0;
+                }
+            }
+
+        }
+
+        #endregion
+
+    }
+}
--- Assignments06_OOp/Program.cs
+++ Assignments06_OOp/Program.cs
@@ -1,10 +1,54 @@
-﻿namespace Assignments06_OOp
+﻿using Assignments06_OOp.First_Project;
+
+namespace Assignments06_OOp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region 2.Override the ToString Function to produce this output:
+
+            //_3DPoint p = new _3DPoint(10, 10, 10);
+            //Console.WriteLine(p.ToString());
+
+            #endregion
+
+            #region 3.Read from the User the Coordinates for 2 points P1, P2 (Check the input using try Pares, Parse, Convert).
+
+            //Console.WriteLine(" enter point P1");
+            //_3DPoint p1 = _3DPoint.Read3DPoint();
+            //Console.WriteLine(" enter point P2");
+            //_3DPoint p2 = _3DPoint.Read3DPoint();
+            //Console.WriteLine(p1);
+            //Console.WriteLine(p2);
+
+            #endregion
+
+            #region Define an array of points and sort this array based on X & Y coordinates.
+
+            int size;
+            bool flag;
+            do
+            {
+                Console.Write("please enter size of array: ");
+                flag =int.TryParse(Console.ReadLine(), out size);
+
+            } while (!flag);
+            _3DPoint[] ThePoint = new _3DPoint[size];
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"Enter cordinate of point {i+1}");
+                ThePoint[i] = _3DPoint.Read3DPoint();
+            }
+            Console.Clear();
+            Console.WriteLine("Array before sort");
+            _3DPoint.Display(ThePoint);
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("Array after sort");
+            _3DPoint.Sortting(ThePoint);
+            _3DPoint.Display(ThePoint); 
+
+            #endregion
         }
     }
 }
