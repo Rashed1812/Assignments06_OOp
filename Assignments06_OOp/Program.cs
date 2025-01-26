cbab432504c73d33874b855082567e41f623add7
Your Name <you@example.com>
2025-01-26 15:10:58 +0200

    Answer Project 02

--- Assignments06_OOp/Program.cs
+++ Assignments06_OOp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Assignments06_OOp.First_Project;
+using Assignments06_OOp.Second_Project;
 
 namespace Assignments06_OOp
 {
@@ -26,28 +27,44 @@ namespace Assignments06_OOp
 
             #region Define an array of points and sort this array based on X & Y coordinates.
 
-            int size;
-            bool flag;
-            do
-            {
-                Console.Write("please enter size of array: ");
-                flag =int.TryParse(Console.ReadLine(), out size);
+            //int size;
+            //bool flag;
+            //do
+            //{
+            //    Console.Write("please enter size of array: ");
+            //    flag =int.TryParse(Console.ReadLine(), out size);
 
-            } while (!flag);
-            _3DPoint[] ThePoint = new _3DPoint[size];
-            for (int i = 0; i < size; i++)
-            {
-                Console.Write($"Enter cordinate of point {i+1}");
-                ThePoint[i] = _3DPoint.Read3DPoint();
-            }
-            Console.Clear();
-            Console.WriteLine("Array before sort");
-            _3DPoint.Display(ThePoint);
-            Console.WriteLine("-------------------------------------------------");
-            Console.WriteLine("Array after sort");
-            _3DPoint.Sortting(ThePoint);
-            _3DPoint.Display(ThePoint); 
+            //} while (!flag);
+            //_3DPoint[] ThePoint = new _3DPoint[size];
+            //for (int i = 0; i < size; i++)
+            //{
+            //    Console.Write($"Enter cordinate of point {i+1}");
+            //    ThePoint[i] = _3DPoint.Read3DPoint();
+            //}
+            //Console.Clear();
+            //Console.WriteLine("Array before sort");
+            //_3DPoint.Display(ThePoint);
+            //Console.WriteLine("-------------------------------------------------");
+            //Console.WriteLine("Array after sort");
+            //_3DPoint.Sortting(ThePoint);
+            //_3DPoint.Display(ThePoint); 
 
+            #endregion
+
+            Maths m = new Maths();
+
+            #region 1.Define Class Maths that has four methods By Object:
+            //Console.WriteLine(m.Add(1 , 2));
+            //Console.WriteLine(m.Subtract(10 , 5));
+            //Console.WriteLine(m.Multiply(3 , 10));
+            //Console.WriteLine(m.Divide(10 , 2)); 
+            #endregion
+
+            #region 2.Modify the program so that you do not have to create an instance of class to call the four methods.
+            Console.WriteLine(Maths.Add(1, 2));
+            Console.WriteLine(Maths.Subtract(10, 5));
+            Console.WriteLine(Maths.Multiply(3, 10));
+            Console.WriteLine(Maths.Divide(10, 2)); 
             #endregion
         }
     }
